b535f9b2973f5a578e46b85f18c0a09b4326e4c2
Ahmed Raza <ahmed.raza@square63.com>
2024-03-01 17:43:07 +0500

    Change over states colors on nav controlss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,12 +48,12 @@
                                         <span id="current"></span>/<span id="total"></span>
                                     </strong>
                                     <div class="custom-navigation flex gap-2">
-                                        <button class="flex-prev hover:bg-[#e20ab7] bg-[#f8c2ed] text-sm text-white w-6 h-6 flex justify-between items-center">
+                                        <button class="flex-prev bg-[#e20ab7] hover:bg-[#f8c2ed] text-sm text-white w-6 h-6 flex justify-between items-center">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 flex-1">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
                                             </svg>
                                         </button>
-                                        <button class="flex-next hover:bg-[#e20ab7] bg-[#f8c2ed] text-sm text-white w-6 h-6 flex justify-between items-center">
+                                        <button class="flex-next bg-[#e20ab7] hover:bg-[#f8c2ed] text-sm text-white w-6 h-6 flex justify-between items-center">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 flex-1">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                                             </svg>
